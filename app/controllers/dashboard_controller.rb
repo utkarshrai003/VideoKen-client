ea427a30e0575d45305efeb5ae90dfa49388afa2
utkarsh <utkarshrai003@gmail.com>
2017-06-28 22:54:54 +0530

    Adds dummy drop down data to the show endpoint

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class DashboardController < ApplicationController
 
   def show
+    @doctors = [{id: 1, name: "Dr. Panchayat"}, {id: 2, name: "Dr. Gulati"}]
+    @patients = [{id: 1, name: "Ram Lal"}, {id: 2, name: "Bilal Saed"}]
   end
 
 end
