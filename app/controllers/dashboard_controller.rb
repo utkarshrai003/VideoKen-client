03d306f78c8ad95918eee7b8d52644ec0c468891
utkarsh <utkarshrai003@gmail.com>
2017-06-29 20:24:38 +0530

    Adds logic to create a user in dashboard controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,4 +5,23 @@ class DashboardController < ApplicationController
     @patients = [{id: 1, name: "Ram Lal"}, {id: 2, name: "Bilal Saed"}]
   end
 
+  def create_user
+    response = ApiCall.new('api/v1/users', patient_params, :post).make_call
+    if response["error"]
+      flash["error"] = response["error"]
+    else
+      flash["error"] = "Created successfully"
+    end
+    redirect_to dashboard_path
+  end
+
+  def create_appointment
+  end
+
+  private
+
+  def patient_params
+    params.permit(:name, :email, :number, :role, :specialization)
+  end
+
 end
