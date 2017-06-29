e89df7b7906c77e4570e639a45ee881750bb572c
utkarsh <utkarshrai003@gmail.com>
2017-06-30 01:49:50 +0530

    Refactored dashboard controller repacing dummy records with actual data

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,12 +1,13 @@
 class DashboardController < ApplicationController
 
   def show
-    @doctors = [{id: 1, name: "Dr. Panchayat"}, {id: 2, name: "Dr. Gulati"}]
-    @patients = [{id: 1, name: "Ram Lal"}, {id: 2, name: "Bilal Saed"}]
+    @patients = ApiCall.new('api/v1/patients', :get).make_call
+    @doctors = ApiCall.new('api/v1/physicians', :get).make_call
+    @appointments = ApiCall.new('api/v1/appointments', :get).make_call
   end
 
   def create_user
-    response = ApiCall.new('api/v1/users', patient_params, :post).make_call
+    response = ApiCall.new('api/v1/users', :post, patient_params).make_call
     if response["error"]
       flash["error"] = response["error"]
     else
@@ -16,12 +17,23 @@ class DashboardController < ApplicationController
   end
 
   def create_appointment
+    response = ApiCall.new('api/v1/appointments', :post, appointment_params).make_call
+    if response["error"]
+      flash["error"] = response["error"]
+    else
+      flash["error"] = "Created successfully"
+    end
+    redirect_to dashboard_path
   end
 
   private
 
   def patient_params
     params.permit(:name, :email, :number, :role, :specialization)
+  end
+
+  def appointment_params
+    params.permit(:patient_id, :physician_id, :diseases)
   end
 
 end
