dbe3e8b70fb0dc19fe057837f5746e92d22febc1
utkarsh <utkarshrai003@gmail.com>
2017-06-30 01:55:02 +0530

    Adds Comments

--- app/helpers/api_call.rb
+++ app/helpers/api_call.rb
@@ -1,3 +1,10 @@
+# Helper class to interact witht the VideKen-server
+# params {
+#   end_url => action name for the endpoint eg. "users" or "appointments"
+#   method => Protocol to be used to make the request eg. "get" or "post"
+#   parametes => Parametes hash to be sent if a POST request
+# }
+
 class ApiCall
   def initialize(end_url, method, parameters=nil)
     @url = ENV["VIDEO_KEN_API_URL"] + "#{end_url}"
